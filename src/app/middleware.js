b170cd47f7689b249e236183b5bd60e273ec6117
Saji <skm.kiani@gmail.com>
2024-11-19 14:49:16 +0330

    middleware

---
+++ src/app/middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+const protectedPaths = ['/profile', '/bookmark', '/search'];
+
+export function middleware(req) {
+  const { pathname, origin } = req.nextUrl;
+  if (protectedPaths.some(path => pathname.startsWith(path))) {
+    const token = req.cookies.get('auth_token');
+    const loginPath = '/auth'
+    const loginUrl = new URL(loginPath, origin);
+    loginUrl.searchParams.append('redirect', pathname);
+
+    if (!token) {
+      return NextResponse.redirect(loginUrl);
+    }
+
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+// Specify the paths that should trigger this middleware
+export const config = {
+  matcher: ['/profile/:path*', '/bookmark/:path*', '/search/:path*'],
+};
